44a7b2748efaaf84a8b7d6df8fff04dbd68c424e
Henrique-William <henriquew2004@gmail.com>
2023-09-27 22:02:59 -0300

    Commit - pensamento component 27/09

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -11,12 +11,19 @@ export class CriarPensamentoComponent implements OnInit {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
+    modelo: 'modelo1'
   }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado!")
+  }
+  cancelarPensamento() {
+    alert("Pensamento Cancelado!")
   }
 
 }
